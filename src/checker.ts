f440c7744a2d9e41b1aca8c5e388685b3b6da2b5
Evgeniy Filatov <efilatov@geeksforless.net>
2018-10-11 17:29:13 +0300

    improved checker

--- src/checker.ts
+++ src/checker.ts
@@ -1,20 +1,26 @@
-import leven from 'leven'
+import * as levenshtein from 'fast-levenshtein'
 
 export interface IAnswer {
 	answer: string
 	index: number
+	distance: number
+}
+
+function editDistance(a: string, b: string): number {
+	return levenshtein.get(a.toLowerCase(), b.toLowerCase())
 }
 
 export function check(answers: string[], guess: string): IAnswer | undefined {
 	const close: string[] = answers.filter(
-		(answer: string) => leven(answer.toLowerCase(), guess.toLowerCase()) < 2
+		(answer: string) => editDistance(answer, guess) < 2
 	)
 
 	if (close.length > 0) {
 		const answer: string = close[0]
 		const index: number = answers.indexOf(answer) + 1
+		const distance: number = editDistance(answer, guess)
 
-		return { answer, index }
+		return { answer, index, distance }
 	}
 
 	return undefined
